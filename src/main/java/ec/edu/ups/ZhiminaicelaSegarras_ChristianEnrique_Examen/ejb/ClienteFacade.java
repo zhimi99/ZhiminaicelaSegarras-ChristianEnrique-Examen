16d1877d07e4265a73afc6691cd46b500e5d014a
zhimi99 <52504869+zhimi99@users.noreply.github.com>
2021-07-27 13:38:19 -0500

    facadeCliente

--- src/main/java/ec/edu/ups/ZhiminaicelaSegarras_ChristianEnrique_Examen/ejb/ClienteFacade.java
+++ src/main/java/ec/edu/ups/ZhiminaicelaSegarras_ChristianEnrique_Examen/ejb/ClienteFacade.java
@@ -1,5 +1,41 @@
 package ec.edu.ups.ZhiminaicelaSegarras_ChristianEnrique_Examen.ejb;
 
-public class ClienteFacade {
+import java.sql.SQLException;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import ec.edu.ups.ZhiminaicelaSegarras_ChristianEnrique_Examen.entidades.Cliente;
+
+@Stateless
+public class ClienteFacade extends AbstractFacade<Cliente>{
+
+	@PersistenceContext(unitName = "ZhiminaicelaSegarras-ChristianEnrique-ExamenPersistenceUnit")
+	private EntityManager em;
+	
+	private Cliente cliente;
+	
+	public ClienteFacade() {
+		super(Cliente.class);
+	}
+
+
+	@Override
+	protected EntityManager getEntityManager() {
+		// TODO Auto-generated method stub
+		return em;
+	}
+	
+	
+	public Cliente search(String cedula)throws SQLException{
+		cliente = new Cliente();
+		String sql = "SELECT cli FROM Cliente cli"
+				+ " WHERE cli.cedula=:cedula";
+		
+		cliente = em.createQuery(sql, Cliente.class).setParameter("cedula", cedula).getSingleResult();
+		
+		return cliente;
+	}
+	
 }
